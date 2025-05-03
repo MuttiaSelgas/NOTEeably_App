fb1bb7d9b9a7a882000135648ee68720996b1177
MuttiaSelgas <selgasmuttia@gmail.com>
2025-05-03 18:15:57 +0800

    pleaseee

--- Noteably-main/frontend/noteably/src/pages/Dashboard/Dashboard.jsx
+++ Noteably-main/frontend/noteably/src/pages/Dashboard/Dashboard.jsx
@@ -33,7 +33,13 @@ function Dashboard() {
         return;
       }
       try {
-        const response = await axiosRequest({ method: 'get', url: API_ENDPOINTS.STUDENT.GET_BY_ID(studentId), ...axiosConfig });
+        const response = await axiosRequest(
+          'get',
+          API_ENDPOINTS.STUDENT.GET_BY_ID(studentId),
+          null,
+          axiosConfig
+        );
+        
         const { studentId: apiStudentId, name, profilePicture } = response.data;
         setStudentData({ 
           studentId: apiStudentId, 
